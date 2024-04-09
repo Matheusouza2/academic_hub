0940f5a5d73545f7a730296b0c633ce93d5fe08d
Gabriel Garcez <ggarcez613@gmail.com>
2024-04-09 13:03:17 -0300

    fix icons exports

---
+++ frontend/src/assets/exports.ts
@@ -0,0 +1,17 @@
+import logo from '../assets/logo.svg'
+import house from '../assets/house.svg'
+import books from '../assets/books.svg'
+import briefcase from '../assets/briefcase.svg'
+import mortarboard from '../assets/mortarboard.svg'
+import book from '../assets/book.svg'
+import chalkboard from '../assets/chalkboard.svg'
+
+export const AppIcons = {
+  'logo': logo,
+  'book': book,
+  'books': books,
+  'briefcase': briefcase,
+  'chalkboard': chalkboard,
+  'house': house,
+  'mortarboard': mortarboard,
+}
