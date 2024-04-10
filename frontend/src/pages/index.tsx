2e35aa3d08faafcedfecba3732bc52d0c9927a05
João Paulo <jhonbrandaoks83@gmail.com>
2024-04-10 11:48:14 -0300

    tela cadastro de curso

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { CreateSubjects } from "./CreateSubjects";
 import { Login } from "./Login";
+import { CadCurso } from "./CadCurso";
 
 export const Pages = {
   Login,
   CreateSubjects,
+  CadCurso,
 }
