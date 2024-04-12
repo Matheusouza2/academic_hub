c4ab50f2e974c0cd6975ef3149c6b2f65de2c4d4
luan0412 <luan0412@outlook.com>
2024-04-12 12:38:57 -0300

    Estilização e complemento do Cadastro de alunos

--- frontend/src/pages/Singup.tsx
+++ frontend/src/pages/Singup.tsx
@@ -1,20 +1,29 @@
 import React,{useState} from "react";
 import { Page } from "./Page";
 import "../styles/Singup.css"
+import { AppIcons } from "../assets/exports";
 export function Singup(){ // Função de cadastro do usuário
 
     const [User, SetUser] =useState({
-        FirstName:"",
-        LastName:"",
+        FullName:"",
         Email:"",
-        Password:""
+        Password:"",
+        Cpf:"",
+        Rg:"",
+        Data:"",
+        Sexo:"",
+        Endereço:""
     })
+    
     type UserType ={
-        FirstName:string,
-        LastName:string,
+        FullName:string,
         Email:string,
-        Password:string
-
+        Password:string,
+        Cpf:string,
+        Rg:string,
+        Data:string,
+        Sexo:String
+        Endereço:string
     }
 
     const [records,setRecords] = useState<UserType[]>([])
@@ -35,31 +44,69 @@ export function Singup(){ // Função de cadastro do usuário
     }
 
     return(
-        <Page>
-            <div className="singup-form">
-                <form action="">
-                    <div className="first-name">
-                        <label htmlFor="">Primeiro nome:</label>
-                        <input type="text" name="FirstName" id="FirstName" value={User.FirstName} onChange={handleChange}/>
+        <Page>  
+            
+            <div className="singup-form flex flex-col h-full gap-6">
+            <h1 className="text-4xl font-bold">Cadastro de Alunos</h1>
+                <form className="flex flex-col w-full h-full gap-6 p-6 bg-white rounded-md shadow-2xl" action=""> 
+               
+                <div className="flex w-full gap-8">
+                    <div className="FullName flex flex-col flex-1 max-w-[300px]">
+                        <label htmlFor="" className="text-gray-900">Nome completo:</label>
+                        <input className="flex w-full h-[47px] bg-gray-500 rounded-md px-2" placeholder="Nome" type="text" name="FullName" id="FullName" value={User.FullName} onChange={handleChange}/>
                     </div>
-                    <div className="last-name">
-                        <label htmlFor="">Sobrenome:</label>
-                        <input type="text" name="LastName" id="LastName" value={User.LastName} onChange={handleChange} />
+                    <div className="Cpf flex flex-col flex-1 max-w-[264px]">
+                        <label htmlFor="">Cpf:</label>
+                        <input className="flexw-full h-[47px] bg-gray-500 rounded-md px-2 "  placeholder="Ex.: 000.000.000-00" type="text" name="Cpf" id="Cpf" value={User.Cpf} onChange={handleChange} />
                     </div>
-                    <div className="email">
-                        <label htmlFor="">Email:</label>
-                        <input type="Email" name="Email" id="Email" value={User.Email} onChange={handleChange}/>
+                    <div className="Rg flex flex-col flex-1 max-w-[264px]">
+                        <label htmlFor="">RG:</label>
+                        <input className="w-full h-[47px] bg-gray-500 rounded-md px-2"  placeholder="Ex.: 0.000.000" type="text" name="Rg" id="Rg" value={User.Rg} onChange={handleChange} />
+                   
                     </div>
-                    <div className="password">
-                        <label htmlFor="">Senha:</label>
-                        <input type="Password" name="Password" id="Password" value={User.Password} onChange={handleChange} />
                     </div>
-                    <button onClick={SubmitHandler}>Cadastrar</button>
 
-                </form>
+
+                    <div className="Data flex gap-8">
+                        <div className="flex flex-col flex-1 max-w-[264px]">
+                            <label htmlFor="">Data de nascimento:</label>
+                            <input className="w-full h-[47px] bg-gray-500 rounded-md px-2"  placeholder="Ex.: dd/mm/aaaa" type="date" name="Data" id="Data" value={User.Data} onChange={handleChange} />
+                        </div>
+                    <div className="flex flex-col flex-1 max-w-[264px]  ">
+                            <label htmlFor="">Sexo:</label>
+                            <input className="w-full h-[20px] bg-gray-500 rounded-md px-2"  placeholder="" type="radio" name="Sexo" id="Male" value="male" onChange={handleChange}/> 
+                            <label htmlFor="Male"> Masculino</label> 
+                             <input className="w-full h-[20px] bg-gray-500 rounded-md px-2"  placeholder="" type="radio" name="Sexo" id="Female" value="female" onChange={handleChange}/> 
+                            <label htmlFor="Female"> Feminino</label>   
+                            </div>
+                            <div className="Endereço flex flex-col flex-1 max-w-[300px]">
+                        <label htmlFor="" className="text-gray-900">Endereço</label>
+                        <input className="flex w-full h-[47px] bg-gray-500 rounded-md px-2" placeholder="Endereço" type="text" name="Endereço" id="Endereço" value={User.Endereço} onChange={handleChange}/>
+                   
+                         </div>
+                    
+                    </div>
+
+                    <div className="Data flex gap-8">
+                    <div className="flex flex-col flex-1 max-w-[264px]">
+                        <label htmlFor="">Email:</label>
+                        <input className="w-full h-[47px] bg-gray-500 rounded-md px-2"  placeholder="Ex.: exemplo@email.com " type="Email" name="Email" id="Email" value={User.Email} onChange={handleChange}/>
+                    </div>
+                    <div className="password flex flex-col flex-1 max-w-[264px]">
+                        <label htmlFor="">Senha:</label>
+                        <input className="w-full h-[47px] bg-gray-500 rounded-md px-2"  placeholder="******" type="Password" name="Password" id="Password" value={User.Password} onChange={handleChange} />
+                    </div>
+                    </div>
+                    <button onClick={SubmitHandler} className="self-end mt-auto w-[264px] hover:opacity-90 transition-all flex items-center justify-center gap-2 rounded-md bg-green text-white py-2">
+                    <img src={AppIcons.disk} alt="Disquete"/>
+                         Cadastrar
+                    
+                         </button>
+                    
+                    </form>
         
-        
-             </div>
+                </div>
+             
         </Page>
     )
 }
