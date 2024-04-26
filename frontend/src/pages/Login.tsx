4563c0af051d930fb82e38e36d6eca591065e575
Anderson <andersonfssantosfel1p3@gmail.com>
2024-04-26 15:03:23 -0300

    atualização da branch

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,44 @@
-import { Page } from "./Page";
+import logo from "/src/assets/logo.svg"
 
 export function Login() {
   return (
-    <Page showSidebar={false}>
+      <>
       <div>Login</div>
-    </Page>
+
+{/* container principal */}
+<div className="containerPrim">
+
+
+  {/* container da imagem */}
+  <div className="flex bg-blue-400">
+  <img src="" alt="" />
+  </div>
+
+  <div>
+    <img src={logo} alt="" />
+  </div>
+
+  <form >  
+    {/* contender dos formularios */}
+    <div className="inputContainer">
+      <label htmlFor="cpf">cpf</label>
+      <input type="text" name="cpf" id="cpf"placeholder="000.000.000-00" />
+
+    </div>
+
+    <div className="inputContainer">
+      <label htmlFor="password">password</label>
+      <input type="password" name="password" id="password"placeholder="***********" />
+
+    </div>
+
+    <button className="button">Login</button>
+
+    <a href="">esqueci minha senha</a>
+
+  </form>
+
+</div>
+</>
   )
 }
