1eb78f3bfdf2191c90bae5a00d7f98a7c5578daf
Eládio Alves <eladiolink1@gmail.com>
2024-04-27 12:17:45 -0300

    Get notas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,33 +20,31 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
 
 
 Route::prefix('v1')->group(function () {
 
-    Route::prefix('user')->group(function () {
+  Route::prefix('user')->group(function () {
+    Route::patch('/edit/{usuario}', [UsuarioController::class, 'update']);
+    Route::post('/cadastrar', [UsuarioController::class, 'store']);
+    Route::delete('delete/{id}', [UsuarioController::class, 'destroy']);
+    Route::get('/list', [UsuarioController::class, 'show']);
+  });
 
-        Route::patch('/edit/{usuario}', [UsuarioController::class, 'update']);
-        Route::post('/cadastrar', [UsuarioController::class, 'store']);
-    });
+  Route::prefix('cursos')->group(function () {
+    Route::post('store', [CursosController::class, 'store']);
+    Route::post('update/{curso}', [CursosController::class, 'update']);
+  });
 
-        Route::delete('delete/{id}', [UsuarioController::class,'destroy']);
-      
-      Route::get('/list', [UsuarioController::class, 'show']);
-
-
-    Route::prefix('cursos')->group(function () {
-        Route::post('store', [CursosController::class, 'store']);
-        // Para alteração de curso:
-        Route::post('update/{curso}', [CursosController::class, 'update']);
-    });
+  Route::prefix('aluno')->group(function () {
+    Route::get('/notas/show/{id}',[AlunoController::class,'showGrades']);
+  });
 
   Route::post('login', [UsuarioController::class, 'validateLogin']);
-  
+
   Route::prefix('disciplinas')->group(function () {
     Route::post('store', [DisciplinaController::class, 'store']);
   });
-  
 });
