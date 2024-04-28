2befdd9228aa6df4c57274b88e014e213eae5e0f
Manoel-vasf <Manoel-vasf@gmail.com>
2024-04-28 10:43:31 -0300

    Realizando a responsividade e ajustes da tela de cadastro de disciplina

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -7,7 +7,7 @@ export function Router() {
       <Route path="/" element={<Pages.Login />} />
       <Route path="/cadastro-disciplinas" element={<Pages.CreateSubjects />} />
       <Route path="/cadastro-curso" element={<Pages.CadCurso />} />
-      <Route path='/cursos/plano_aprovacao' element={<Pages.ScreenReviewApproval />} />
+      <Route path='/cursos/plano_ aprovacao' element={<Pages.ScreenReviewApproval />} />
       <Route path="/notas" element={<Pages.QueryGrades />} />
       <Route path="/DashboardAluno" element={<Pages.DashboardAluno/>} />
     </Routes>
--- frontend/src/pages/CreateSubjects.tsx
+++ frontend/src/pages/CreateSubjects.tsx
@@ -10,50 +10,61 @@ export function CreateSubjects() {
   }
 
   return (
-    <Page>
-      <div className="flex flex-col h-[91%] gap-6">
-        <h1 className="text-4xl font-bold">Disciplinas / Cadastrar nova disciplina</h1>
+    <Page typeSidebar="teacher">
+      
+      <div className="flex flex-col gap-1">
+        
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">Cadastrar nova disciplina</h1>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full h-full gap-6 p-6 bg-white rounded-md shadow-2xl">
-          <div className="flex w-full gap-8">
-            <div className="flex flex-col flex-1 max-w-[560px]">
-              <label htmlFor="name" className="text-gray-900 w-fit">Nome</label>
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full h-sc p-6 bg-white rounded-md shadow-2xl">
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 px-6 gap-3">
+  
+            <div className="col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-2 xl:col-span-2">
+              <label htmlFor="name" className="text-gray-900">Nome</label>
               <input {...register('name')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="name" placeholder="Nome da disciplina" />
             </div>
-            <div className="flex flex-col flex-1 max-w-[264px]">
+
+            <div className="col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-2 xl:col-span-1">
               <label htmlFor="sigla" className="text-gray-900">Sigla</label>
-              <input {...register('sigla')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="sigla" placeholder="Nome da disciplina" />
+              <input {...register('sigla')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="sigla" placeholder="Sigla da disciplina" />
             </div>
-            <div className="flex flex-col flex-1 max-w-[264px]">
+
+            <div className="col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-2 xl:col-span-1">
               <label htmlFor="code" className="text-gray-900">Código</label>
-              <input {...register('code')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="code" placeholder="Nome da disciplina" />
+              <input {...register('code')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="code" placeholder="Código da disciplina" />
             </div>
-          </div>
 
-          <div className="flex gap-8">
-            <div className="flex flex-col flex-1 max-w-[264px]">
+            <div className="col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-2 xl:col-span-1">
               <label htmlFor="ch_teory" className="text-gray-900">Carga horária teórica</label>
-              <input {...register('ch_teory')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="ch_teory" placeholder="Nome da disciplina" />
+              <input {...register('ch_teory')} type="number" min={0} className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="ch_teory" placeholder="Carga horária teórica da disciplina" />
             </div>
-            <div className="flex flex-col flex-1 max-w-[264px]">
+
+            <div className="col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-2 xl:col-span-1">
               <label htmlFor="ch_pratical" className="text-gray-900">Carga horária prática</label>
-              <input {...register('ch_pratical')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="ch_pratical" placeholder="Nome da disciplina" />
+              <input {...register('ch_pratical')} type="number" min={0} className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="ch_pratical" placeholder="Carga horária prática da disciplina" />
             </div>
-            <div className="flex flex-col flex-1 max-w-[264px]">
+
+            <div className="col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-2 xl:col-span-2">
               <label htmlFor="course" className="text-gray-900">Curso</label>
-              <input {...register('course')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="course" placeholder="Nome da disciplina" />
+              <select {...register('course')} id="course" name="Curso" className="w-full h-[47px] bg-gray-500 rounded-md px-2 text-gray-900">
+                <option value="" selected>--- Cursos ---</option>
+              </select>
+            </div>
+
+            <div className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-4 xl:col-span-4">
+              <label htmlFor="ementa" className="text-gray-900">Ementa</label>
+              <textarea {...register('ementa')} id="ementa" className="w-full h-[100px] lg:h-[160px] px-2 bg-gray-500 rounded-md resize-none " placeholder="Digite a ementa da disciplina"></textarea>
             </div>
           </div>
 
-          <div className="flex flex-col flex-1 max-w-[860px]">
-            <label htmlFor="ementa" className="text-gray-900">Ementa</label>
-            <textarea {...register('ementa')} id="ementa" className="w-full h-full p-2 bg-gray-500 rounded-md resize-none"></textarea>
-          </div>
+          <div className="w-full flex justify-end mt-10 lg:mt-20 pr-6">
+              <button type="button" className="hover:opacity-60 flex items-center justify-center gap-2 rounded-md bg-green text-white py-2 px-4">
+                <img src={AppIcons.disk} alt="Disquete" />
+                Salvar
+              </button>
+            </div> 
 
-          <button type="submit" className="self-end mt-auto w-full max-w-[264px] hover:opacity-90 transition-all flex items-center justify-center gap-2 rounded-md bg-green text-white py-2">
-            <img src={AppIcons.disk} alt="Disquete" />
-            Salvar
-          </button>
         </form>
       </div>
     </Page>
