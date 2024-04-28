ccdfa6a65f1b5ddc0334fcacee8163ffd3f02d57
Lucas Allan <lucasallan.contato@gmail.com>
2024-04-28 11:59:07 -0300

    Ajuste na responsividade da page Alteração de Cursos

--- frontend/src/pages/CadCurso.tsx
+++ frontend/src/pages/CadCurso.tsx
@@ -15,37 +15,45 @@ export function CadCurso() {
       }
 
         return (
-            <Page>
-                <div className="flex flex-col h-[91%] gap-6">
+            <Page typeSidebar="admin">
+                <div className="flex flex-col w-[96%] h-[91%] gap-6 sm: h-[80%] md:h-[84%]">
                     <h1 className="text-4xl font-bold">Cursos / Alterar curso</h1>
 
                     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full h-full gap-6 p-6 bg-white rounded-md shadow-2xl">
-                        <div className="flex gap-8">
+                        
+                        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6">
+
                             <div className="flex flex-col">
-                                <label htmlFor="name" className="text-gray-900">Nome</label>
+                                <label htmlFor="name" className="text-gray-900"> Nome</label>
                                 <input {...register('nome')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="name" placeholder="Nome do curso" />
                             </div>
-                            <div className="flex flex-col flex-1 max-w-[264px]">
-                                <label htmlFor="sigla" className="text-gray-900">Carga Horária</label>
+
+                            <div className="flex flex-col">
+                                <label htmlFor="sigla" className="text-gray-900"> Carga Horária</label>
                                 <input {...register('carga_horaria')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="sigla" placeholder="000 Horas" />
                             </div>
-                            <div className="flex flex-col flex-1 max-w-[264px]">
-                                <label htmlFor="code" className="text-gray-900">Coordenador</label>
+
+                            <div className="flex flex-col">
+                                <label htmlFor="code" className="text-gray-900"> Coordenador</label>
                                 <input {...register('coordenador_id')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="code" placeholder="Selecione" />
                             </div>
-                        </div>
 
-                        <div className="flex gap-8">
-                            <div className="flex flex-col flex-1">
+                            <div className="flex flex-col ">
                                 <label htmlFor="ch_teory" className="text-gray-900">Sigla</label>
                                 <input {...register('sigla')} type="text" className="w-full h-[47px] bg-gray-500 rounded-md px-2" id="ch_teory" placeholder="Uma sigla para representar o curso" />
                             </div>
+
                         </div>
 
-                        <button type="submit" className="self-end mt-auto w-[264px] hover:opacity-90 transition-all flex items-center justify-center gap-2 rounded-md bg-green text-white py-2">
-                            <img src={AppIcons.disk} alt="Disquete" />
-                            Salvar
-                        </button>
+                        <div className="flex justify-end mt-4"> 
+
+                            <button type="submit" className="w-[200px] bg-green hover:bg-opacity-80 focus:brightness-90 transition-all flex items-center justify-center gap-2 rounded-md text-white py-2">
+                                <img src={AppIcons.disk} alt="Disquete" />
+                                    Salvar
+                            </button>
+
+                        </div>
+
                     </form>
                 </div>
             </Page>
