607de16b952128e282f63a1ba72847727fa3ccde
Matheus Souza <matheusouza1997@gmail.com>
2024-05-03 14:56:08 -0300

    merge

--- backend/app/Http/Controllers/AlunoController.php
+++ backend/app/Http/Controllers/AlunoController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Aluno;
 use Illuminate\Http\Request;
 
 class AlunoController extends Controller
-{   
+{
     public function store(Request $request)
     {
         dd($request->aluno);
+
+
 
         return response()->json(['message' => 'aluno cadastrado com sucesso !'], 200);
     }
