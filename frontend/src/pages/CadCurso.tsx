1b2b16db398da66fba56ceb0cf46591a134d7128
EmafPlayer <emanuelfoliveira05@gmail.com>
2024-05-31 22:40:33 -0300

    Concluindo Task de Validação da tela de Cadastro de curso

--- frontend/src/pages/CadCurso.tsx
+++ frontend/src/pages/CadCurso.tsx
@@ -1,10 +1,14 @@
+import { useParams } from "react-router-dom";
 import { FormularioCurso } from "../components";
 import { Page } from "./Page";
 
 export function CadCurso() {
+
+    const { curso_id } = useParams();
+
     return (
         <Page typeSidebar="admin">
-            <FormularioCurso tipo="Cadastrar"/>
+            <FormularioCurso curso_id={curso_id ? Number.parseInt(curso_id,10) : null}/>
         </Page>
     )
 }
